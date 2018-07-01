bdb760966018d972e632204dd615e330cb516061
adir-ch <actiger@gmail.com>
2018-07-01 21:20:55 +1000

    added ver

--- main.go
+++ main.go
@@ -5,8 +5,9 @@ import (
 	"time"
 )
 
+var ver = "1.0"
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Printf("hello world (version: %s)\n", ver)
 	fmt.Println("current time:", time.Now().Format(time.RFC3339))
-	fmt.Println("math calc: 1+1 =", (1 + 1))
 }
